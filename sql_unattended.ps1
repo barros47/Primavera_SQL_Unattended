97b99c0b9f927853cf6c5b56696cdb0aa03edb56
barros47 <109518888+barros47@users.noreply.github.com>
2023-01-10 16:14:08 +0100

    Add files via upload

---
+++ sql_unattended.ps1
@@ -0,0 +1,12 @@
+﻿Start-Process powershell.exe -Verb RunAs "-command NetSh Advfirewall set allprofiles state on";
+
+md c:\Gestao;
+md c:\Gestao\install;
+md c:\Gestao\backup;
+md c:\Gestao\primavera;
+
+Copy-Item "ConfigurationFile.ini" "c:\Gestao\install\";
+
+Install-WindowsFeature NET-Framework-Features;
+
+Start-Process -FilePath “C:\SCCM\SQLEXPRADV_x64_ENU\SETUP.exe /CONFIGURATIONFILE=C:\Gestao\install\ConfigurationFile.ini /IAcceptSQLServerLicenseTerms”
