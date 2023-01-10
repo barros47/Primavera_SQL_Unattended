8d8cae4d17ac0308159a3781f95f07d8b6b2bd54
barros47 <109518888+barros47@users.noreply.github.com>
2023-01-10 16:24:50 +0100

    Update sql_unattended.ps1

--- sql_unattended.ps1
+++ sql_unattended.ps1
@@ -1,12 +1,12 @@
-﻿Start-Process powershell.exe -Verb RunAs "-command NetSh Advfirewall set allprofiles state on";
+Start-Process powershell.exe -Verb RunAs "-command NetSh Advfirewall set allprofiles state on";
 
 md c:\Gestao;
 md c:\Gestao\install;
 md c:\Gestao\backup;
 md c:\Gestao\primavera;
 
-Copy-Item "ConfigurationFile.ini" "c:\Gestao\install\";
+Copy-Item -Path  "C:\SCCM\ConfigurationFile.ini" "c:\Gestao\install\";
 
-Install-WindowsFeature NET-Framework-Features;
+Add-WindowsCapability –Online -Name NetFx3~~~~ –Source D:\sources\sxs;
 
-Start-Process -FilePath “C:\SCCM\SQLEXPRADV_x64_ENU\SETUP.exe /CONFIGURATIONFILE=C:\Gestao\install\ConfigurationFile.ini /IAcceptSQLServerLicenseTerms”
+Start-Process -FilePath C:\SCCM\sqlexpress2017\SETUP.exe  -Verb RunAs  /CONFIGURATIONFILE="C:\Gestao\install\ConfigurationFile.ini"
